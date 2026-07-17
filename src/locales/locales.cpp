20a20e473d200f0866bf7db04a94022910da4d38
Wusiki Jeronii <wusikijeronii@gmail.com>
2026-07-17 14:24:12 +0300

    [Win32] Locale OS based

--- src/locales/locales.cpp
+++ src/locales/locales.cpp
@@ -29,7 +29,27 @@ namespace acul
 
         string get_user_language(const char **pLanguages, size_t count)
         {
-            string shell_locale = setlocale(LC_MESSAGES, "");
+            auto find_supported = [pLanguages, count](const string &language) -> string {
+                for (size_t i = 0; i < count; ++i)
+                    if (language == pLanguages[i]) return pLanguages[i];
+                return {};
+            };
+
+#ifdef _WIN32
+            wchar_t locale_name[LOCALE_NAME_MAX_LENGTH]{};
+            wchar_t language_name[LOCALE_NAME_MAX_LENGTH]{};
+            if (GetUserDefaultLocaleName(locale_name, LOCALE_NAME_MAX_LENGTH) > 0 &&
+                GetLocaleInfoEx(locale_name, LOCALE_SISO639LANGNAME, language_name, LOCALE_NAME_MAX_LENGTH) > 0)
+            {
+                string language;
+                for (const wchar_t *it = language_name; *it != L'\0'; ++it)
+                    language.push_back(static_cast<char>(*it));
+                if (auto supported = find_supported(language); !supported.empty()) return supported;
+            }
+#endif
+
+            const char *resolved_locale = setlocale(LC_MESSAGES, "");
+            string shell_locale = resolved_locale ? resolved_locale : "";
             size_t language_end = shell_locale.size();
             for (const char *separator : {"_", ".", "-"})
             {
@@ -37,8 +57,7 @@ namespace acul
                 if (position != SIZE_MAX && position < language_end) language_end = position;
             }
             const string short_locale = shell_locale.substr(0, language_end);
-            for (size_t i = 0; i < count; ++i)
-                if (short_locale == pLanguages[i]) return pLanguages[i];
+            if (auto supported = find_supported(short_locale); !supported.empty()) return supported;
             return count > 0 ? pLanguages[0] : string{};
         }
     } // namespace locales
